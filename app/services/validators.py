c419da8ed5e5b18c7a3a55c9738d7f0dc5a1a278
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-11-23 13:20:30 +0300

    Fix validators.py

--- app/services/validators.py
+++ app/services/validators.py
@@ -1,9 +1,9 @@
-INVALID_ROW_SHEET_ROW_COUNT = (
+INVALID_SHEET_ROW_COUNT = (
     'Данные для обновления превышают допустимое количество строк. '
     'Допустимо: {sheet_row_count}. '
     'Требуется: {insert_row_count}.'
 )
-INVALID_COLUMN_SHEET_ROW_COUNT = (
+INVALID_SHEET_COLUMN_COUNT = (
     'Данные для обновления превышают допустимое количество столбцов. '
     'Допустимо: {sheet_column_count}. '
     'Требуется: {insert_column_count}.'
@@ -15,7 +15,7 @@ def check_row_count(
 ) -> None:
     if insert_row_count > sheet_row_count:
         raise ValueError(
-            INVALID_ROW_SHEET_ROW_COUNT.format(
+            INVALID_SHEET_ROW_COUNT.format(
                 sheet_row_count=sheet_row_count,
                 insert_row_count=insert_row_count
             )
@@ -27,7 +27,7 @@ def check_column_count(
 ) -> None:
     if insert_column_count > sheet_column_count:
         raise ValueError(
-            INVALID_COLUMN_SHEET_ROW_COUNT.format(
+            INVALID_SHEET_COLUMN_COUNT.format(
                 sheet_column_count=sheet_column_count,
                 insert_column_count=insert_column_count
             )
